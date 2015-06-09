9a1e28c50fc7a80393e4e8c661b7cff1823d4860
colinsr <colinsr@me.com>
2015-06-09 14:13:34 -0400

    WIP setup noteboxes

---
+++ src/noteboxes.js
@@ -0,0 +1,7 @@
+export class Noteboxes{
+  //Getters can't be observed with Object.observe, so they must be dirty checked.
+  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
+  //To optimize by declaring the properties that this getter is computed from, uncomment the line below.
+  //@computedFrom('firstName', 'lastName')
+  
+}
